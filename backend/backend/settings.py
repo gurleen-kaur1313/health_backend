73bfb1ac345e94aa1ac8f6913e78569aec371b88
Harmanjit Singh <harmanjit140500@gmail.com>
2021-03-28 12:50:33 +0530

    idk

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = bool(os.environ.get("DEBUG", 'True'))
 
 ALLOWED_HOSTS = [
-    'localhost', 'Veehacks-backend.ap-south-1.elasticbeanstalk.com', '127.0.0.1:8000']
+    'localhost', 'veehacksbackend.ap-south-1.elasticbeanstalk.com', '127.0.0.1:8000']
 
 
 # Application definition
