64713763a5edb7b61c3ba45c81ed592bed69c554
Harmanjit Singh <harmanjit140500@gmail.com>
2021-03-28 11:40:41 +0530

    aws2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -146,4 +146,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
